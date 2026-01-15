10ca9bf0572d3697b19c676c3c0559d552f0b79e
ISHariharan <ishariharan2004@gmail.com>
2026-01-15 21:01:55 +0530

    Change : Evolyn Data Service Creation

---
+++ src/main/java/com/evolyn/evolyn_data_service/EvolynDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evolyn.evolyn_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolynDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolynDataServiceApplication.class, args);
+	}
+
+}
